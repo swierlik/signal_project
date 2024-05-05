cf2fa4e724fa102f404b79b7a3930b0a3a346699
swierlik <igorswierlik@gmail.com>
2024-05-05 15:46:05 +0200

    Task2 Finished

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -1,7 +1,11 @@
 package com.alerts;
 
+import java.util.List;
+
+import com.data_management.DataReaderReal;
 import com.data_management.DataStorage;
 import com.data_management.Patient;
+import com.data_management.PatientRecord;
 
 /**
  * The {@code AlertGenerator} class is responsible for monitoring patient data
@@ -11,6 +15,14 @@ import com.data_management.Patient;
  */
 public class AlertGenerator {
     private DataStorage dataStorage;
+    public static void main(String[] args) {
+        DataReaderReal reader = new DataReaderReal();
+        DataStorage dataStorage = new DataStorage();
+        reader.readData(dataStorage);
+        AlertGenerator alertGenerator = new AlertGenerator(dataStorage);
+        Patient patient = new Patient(53);
+        alertGenerator.evaluateData(patient);
+    }
 
     /**
      * Constructs an {@code AlertGenerator} with a specified {@code DataStorage}.
@@ -34,8 +46,136 @@ public class AlertGenerator {
      *
      * @param patient the patient data to evaluate for alert conditions
      */
+
     public void evaluateData(Patient patient) {
+        // Get all records for the patient
+        List<PatientRecord> records = patient.getRecords(17128316L,171283163844400L);
+
         // Implementation goes here
+        int bpSystCounter = 0;
+        PatientRecord bpSystLastRecord = null;
+        int bpDiastCounter = 0;
+        PatientRecord bpDiastLastRecord = null;
+
+        int saturationCounter = 0;
+        double lastSaturation = 0.0;
+        long lastSaturationTimestamp = 0;
+
+        boolean lowSystolic = false;
+        boolean lowSaturation = false;
+
+        int heartRateCounter = 0;
+        double lastHeartRate = 0.0;
+        boolean irregularBeatDetected = false;
+        
+        for (PatientRecord record : records) {
+            if (record.getRecordType().equals("SystolicPressure")) {
+                if (bpSystLastRecord != null) {
+                    double systolicDifference = record.getMeasurementValue() - bpSystLastRecord.getMeasurementValue();
+                    if (Math.abs(systolicDifference) > 10) {
+                        bpSystCounter++;
+                    } else {
+                        bpSystCounter = 0; // Reset counter if difference is not significant
+                    }
+                    if (bpSystCounter >= 2) {
+                        Alert trendAlert = new Alert(String.valueOf(record.getPatientId()), "Systolic Trend Alert", record.getTimestamp());
+                        triggerAlert(trendAlert);
+                    }
+                }
+                //Check for low systolic pressure
+                if (record.getMeasurementValue() < 90) {
+                    lowSystolic = true;
+                } else {
+                    lowSystolic = false;
+                }
+
+                bpSystLastRecord = record;
+            } else if (record.getRecordType().equals("DiastolicPressure")) {
+                if (bpDiastLastRecord != null) {
+                    double diastolicDifference = record.getMeasurementValue() - bpDiastLastRecord.getMeasurementValue();
+                    if (Math.abs(diastolicDifference) > 10) {
+                        bpDiastCounter++;
+                    } else {
+                        bpDiastCounter = 0; // Reset counter if difference is not significant
+                    }
+                    if (bpDiastCounter >= 2) {
+                        Alert trendAlert = new Alert(String.valueOf(record.getPatientId()), "Diastolic Trend Alert", record.getTimestamp());
+                        triggerAlert(trendAlert);
+                    }
+                }
+
+                bpDiastLastRecord = record;
+            } else if (record.getRecordType().equals("Saturation")) {
+                double saturation = record.getMeasurementValue();
+        
+                // Check for low saturation
+                if (saturation < 92) {
+                    lowSaturation = true;
+                    Alert lowSaturationAlert = new Alert(String.valueOf(record.getPatientId()), "Low Saturation Alert", record.getTimestamp());
+                    triggerAlert(lowSaturationAlert);
+                }
+                else {
+                    lowSaturation = false;
+                }
+        
+                // Check for rapid drop
+                if (lastSaturationTimestamp != 0 && (record.getTimestamp() - lastSaturationTimestamp) <= 600000) {
+                    double saturationDrop = lastSaturation - saturation;
+                    if (saturationDrop >= 5) {
+                        Alert rapidDropAlert = new Alert(String.valueOf(record.getPatientId()), "Rapid Saturation Drop Alert", record.getTimestamp());
+                        triggerAlert(rapidDropAlert);
+                    }
+                }
+        
+                lastSaturation = saturation;
+                lastSaturationTimestamp = record.getTimestamp();
+            } else if (record.getRecordType().equals("ECG")) {
+                double heartRate = record.getMeasurementValue();
+        
+                // Check for abnormal heart rate
+                if (heartRate < 50 || heartRate > 100) {
+                    Alert abnormalHeartRateAlert = new Alert(String.valueOf(record.getPatientId()), "Abnormal Heart Rate Alert", record.getTimestamp());
+                    triggerAlert(abnormalHeartRateAlert);
+                }
+        
+                // Check for irregular beat patterns
+                if (lastHeartRate != 0.0) {
+                    double heartRateDifference = Math.abs(heartRate - lastHeartRate);
+                    if (heartRateDifference > 0.2 * lastHeartRate) {
+                        irregularBeatDetected = true;
+                    } else {
+                        irregularBeatDetected = false;
+                    }
+                }
+        
+                if (irregularBeatDetected) {
+                    Alert irregularBeatAlert = new Alert(String.valueOf(record.getPatientId()), "Irregular Beat Alert", record.getTimestamp());
+                    triggerAlert(irregularBeatAlert);
+                }
+        
+                lastHeartRate = heartRate;
+            } else if (record.getRecordType().equals("Alert")&&Math.abs(1-record.getMeasurementValue())<0.01)
+            {
+                Alert alert = new Alert(String.valueOf(record.getPatientId()), "Alert Triggered (Manual)", record.getTimestamp());
+                triggerAlert(alert);
+            }
+            
+            // Check for critical thresholds
+            if (record.getRecordType().equals("SystolicPressure") || record.getRecordType().equals("DiastolicPressure")) {
+                if (record.getMeasurementValue() > 180 || record.getMeasurementValue() < 90) {
+                    Alert thresholdAlert = new Alert(String.valueOf(record.getPatientId()), "Critical Threshold Alert", record.getTimestamp());
+                    triggerAlert(thresholdAlert);
+                }
+            }
+
+            // Check for Hypotensive Hypoxemia Alert
+            if (lowSystolic && lowSaturation) {
+                Alert hypotensiveHypoxemiaAlert = new Alert(String.valueOf(record.getPatientId()), "Hypotensive Hypoxemia Alert", record.getTimestamp());
+                triggerAlert(hypotensiveHypoxemiaAlert);
+            }
+        }
+        
+
     }
 
     /**
@@ -48,5 +188,6 @@ public class AlertGenerator {
      */
     private void triggerAlert(Alert alert) {
         // Implementation might involve logging the alert or notifying staff
+        System.out.println("Alert triggered: " + alert.getPatientId() + " - " + alert.getCondition() + " - " + alert.getTimestamp());
     }
 }
---
+++ src/main/java/com/data_management/DataReaderReal.java
@@ -0,0 +1,71 @@
+package com.data_management;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DataReaderReal implements DataReader{
+    @Override
+    public void readData(DataStorage dataStorage) {
+
+        File directory = new File("C:\\Users\\igors\\OneDrive\\Dokumenty\\##Studia\\Rok1\\Programowanie\\Period5\\signal_project\\output");
+
+        // Get all files in the directory
+        File[] files = directory.listFiles();
+        
+
+        for(File file : files){
+            String data;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                StringBuilder content = new StringBuilder();
+                String line;
+                // Read each line from the file and append it to the StringBuilder
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append("\n");
+                }
+                data = content.toString();
+            }
+            catch (IOException e) {
+                System.err.println("Error reading file: " + file.getName());
+                continue;
+            }
+
+            Pattern pattern = Pattern.compile("Patient ID: (\\d+), Timestamp: (\\d+), Label: (.*), Data: (\\d+\\.\\d+)");
+
+            
+            Matcher matcher = pattern.matcher(data);
+
+            // Iterate over matches and extract variables
+            while (matcher.find()) {
+                int patientID = Integer.parseInt(matcher.group(1));
+                long timestamp = Long.parseLong(matcher.group(2));
+                String label = matcher.group(3);
+                double dataValue;
+                if(file.getName().equals("Saturation.txt"))//saturation and alert are strings, rest doubles
+                    dataValue = Double.parseDouble(matcher.group(4).substring(0, matcher.group(4).length()-1));
+                else if(file.getName().equals("Alert.txt"))
+                    dataValue = matcher.group(4).equals("triggered") ? 1 : 0; 
+                else
+                    dataValue = Double.parseDouble(matcher.group(4));
+                // Read the data from the file
+                // Store the data in the data storage
+                dataStorage.addPatientData(patientID, dataValue, label, timestamp);
+                System.out.println("Patient ID: " + patientID + ", Timestamp: " + timestamp + ", Label: " + label + ", Data: " + dataValue);
+            }
+        }
+        //System.out.println(Arrays.toString(files));
+    }
+    public static void main(String[] args) {
+        DataReaderReal reader = new DataReaderReal();
+        DataStorage dataStorage = new DataStorage();
+        reader.readData(dataStorage);
+    }
+
+    
+}
